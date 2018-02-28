00daf73a9385a455a27ab76e7aa59c0cdcb0f390
Novikov German <ip.novikov.smolensk@gmail.com>
2018-02-28 08:51:39 +0300

    exam1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+import App from './App.vue';
+
+const app = new Vue({
+
+    render(h) {
+        return h(App);
+    },
+});
+
+app.$mount(document.querySelector('#app'));
